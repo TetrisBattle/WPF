be89d031a6a7faaec95c9da24856f8aca00e3791
TetrisBattle <thien5596@gmail.com>
2025-02-26 05:48:43 +0200

    Add project files.

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using HelloWorld.Pages;
+using MahApps.Metro.Controls;
+
+namespace HelloWorld;
+
+public partial class MainWindow : MetroWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        MainFrame.Navigate(new HomePage());
+    }
+}
